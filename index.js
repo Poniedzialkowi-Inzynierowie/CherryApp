c79e96bea8abfc5d36b5a86a16935a9c073b45d6
Kacper <adamn.zxc@gmial.com>
2017-09-30 16:37:20 +0200

    Added routes for static files

--- index.js
+++ index.js
@@ -1,10 +1,47 @@
-const express = require('express')
-const app = express()
+const fs = require('fs');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+const port = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(cookieParser());
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+
+getFile(path) -> {
+	fs.readFile('static/app.min.js', 'utf8', (err, data) => {
+		if (err) { console.log("Can't read file due to: "+ err)}
+		else{ return data }
+	}
+}
+
+
+// ----------------------------------------------------------------------------
+// As project is using REST api philosophy, we do not need
+// any dynamic acces to static files and so we provide staticly
+// typed routes to main.min.css and app.min.js
+
+app.route('/static')
+.get((req, res) => {
+	switch(req.param("file")){
+		case 'main.min.css':
+			res.send( getFile('static/main.min.css') );
+			break;
+		case 'app.min.js':
+			res.send( getFile('static/main.min.css') );
+			break;
+		default:
+			console.log(
+				"Warning: Requested static", req.param("file"), "file",
+				"wich doesn't exist, or is not served by server"
+			)
+	}
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+// ----------------------------------------------------------------------------
+app.listen(port, () => {
+	console.log('Server started! At http://localhost:' + port)
+});
