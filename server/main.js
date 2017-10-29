cd7cf7f835ccbf4e7c720cf7448926da636aeca2
Jan <j.szachno@gmail.com>
2017-10-29 20:13:08 +0100

    added compress middleware to server

--- server/main.js
+++ server/main.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const compression = require('compression');
 
 const port = process.env.PORT || 8080;
 
 const app = express();
 
 app.use(express.static('build'));
+// gzip returned files for better speed
+app.use(compression());
 
 app.listen(port);
