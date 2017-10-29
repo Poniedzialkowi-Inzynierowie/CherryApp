6aefe7171e4cae54a44eba6e9dbd430ff4b5e198
Jan <j.szachno@gmail.com>
2017-10-29 03:11:17 +0100

    general code cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,20 @@
+// util
 const gulp = require('gulp');
-const sass = require('gulp-sass');
-const uglify = require('gulp-uglify');
-const autoprefixer = require('gulp-autoprefixer');
 const sourcemaps = require('gulp-sourcemaps');
+const notify = require('gulp-notify');
+const del = require('del');
+// styles
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+// scripts
 const browserify = require('browserify');
+const uglify = require('gulp-uglify');
 const source = require('vinyl-source-stream');
 const buffer = require('vinyl-buffer');
+// server
 const exec = require('child_process').exec;
-const del = require('del');
-const notify = require('gulp-notify');
 
-
+// Paths to source files
 const src = {
 	serverEntry: 'server/main.js',
 	scriptsEntry: 'src/index.js',
@@ -22,6 +26,7 @@ const src = {
 	assets: 'src/assets/**/*',
 }
 
+// Paths to built files destinations
 const dest = {
 	scripts: 'build',
 	worker: 'build',
@@ -29,25 +34,33 @@ const dest = {
 	assets: 'build/assets',
 }
 
-gulp.task('clean_scripts', () => del(dest.scripts));
+gulp.task('default', ['build', 'server', 'watch']);
 
-gulp.task('clean_styles', () => del(dest.styles));
+gulp.task('build', ['scripts', 'styles', 'assets']);
 
-gulp.task('clean_assets', () => del(dest.assets));
+gulp.task('clean', () => del('build'));
 
 gulp.task('assets', ['clean_assets'], () => {
 	return gulp.src(src.assets)
 		.pipe(gulp.dest(dest.assets));
 })
 
-gulp.task('server', () => {
-	exec(`node ${src.serverEntry}`, (err, stdout, stderr) => {
-		console.log(stdout);
-		console.log(stderr);
-	});
+gulp.task('clean_assets', () => del(dest.assets));
+
+gulp.task('styles', ['clean'], () => {
+	return gulp.src(src.styles)
+		.pipe(sourcemaps.init())
+		.pipe(sass({outputStyle: 'compressed'})
+			.on('error', sass.logError))
+		.pipe(autoprefixer({
+			browsers: ['last 4 versions'],
+			cascade: true,
+		}))
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest(dest.styles));
 });
 
-gulp.task('scripts', ['clean_scripts'], () => {
+gulp.task('scripts', ['clean'], () => {
 	const b = browserify(src.scriptsEntry, {debug: true})
 		.transform('babelify', {sourceMaps: true})
 
@@ -64,40 +77,18 @@ gulp.task('scripts', ['clean_scripts'], () => {
 		.pipe(gulp.dest(dest.scripts))
 });
 
-gulp.task('styles', ['clean_styles'], () => {
-	return gulp.src(src.styles)
-		.pipe(sourcemaps.init())
-		.pipe(sass({outputStyle: 'compressed'})
-			.on('error', sass.logError))
-		.pipe(autoprefixer({
-			browsers: ['last 4 versions'],
-			cascade: true,
-		}))
-		.pipe(sourcemaps.write())
-		.pipe(gulp.dest(dest.styles));
+gulp.task('server', () => {
+	exec(`node ${src.serverEntry}`, (err, stdout, stderr) => {
+		console.log(stdout);
+		console.log(stderr);
+	});
 });
 
-// Rerun the task when a file changes
-gulp.task('watch', function() {
-	gulp.watch(	paths.scripts,		['scripts']	);
-	gulp.watch(	paths.styles,		['styles']	);
-	gulp.watch(	paths.images,		['images']	);
+gulp.task('watch', () => {
+	gulp.watch(src.scripts, ['scripts']);
+	gulp.watch(src.styles, ['styles']);
+	gulp.watch(src.assets, ['assets']);
 });
-
-// The `default` task (called when you run `gulp` from cli)
-gulp.task('default', ['watch', 'build', 'server']);
-
-// The `build` task moves and compiles files to end folder;
-// (called when you run `gulp build` from cli)
-gulp.task('build', ['scripts', 'styles', 'images']);
-
-// The `clean` task removes files from end folder;
-// (called when you run `gulp clean` from cli)
-gulp.task('clean', [
-	'clean_scripts',
-	'clean_styles',
-	'clean_images',
-]);
 
 const handleError = function (...args) {
   notify.onError({
