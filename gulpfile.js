85a07f53fdbd8d0d2a284fd988bd6d47244e3fe2
Jan <j.szachno@gmail.com>
2017-10-29 20:44:26 +0100

    added production build option to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 // util
 const gulp = require('gulp');
+const gutil = require('gulp-util');
 const sourcemaps = require('gulp-sourcemaps');
 const notify = require('gulp-notify');
 const del = require('del');
@@ -14,15 +15,19 @@ const buffer = require('vinyl-buffer');
 // server
 const exec = require('child_process').exec;
 
-// TODO: add conditional builds to disable sourcemaps in production
 // TODO: add heroku server build to package.json
 // TODO: add live browser reload on file change
 // TODO: add static analysis tools to project
 // TODO: setup unit testing for server and components
 
+const config = {
+	server: 'server/main.js',
+	// to run producton build use `gulp build --production`
+	production: !!gutil.env.production,
+};
+
 // Paths to source files
 const src = {
-	serverEntry: 'server/main.js',
 	scriptsEntry: 'src/index.js',
 	htmlEntry: 'src/index.html',
 	scripts: [
@@ -31,7 +36,7 @@ const src = {
 	],
 	styles: 'src/styles/**/*.scss',
 	assets: 'src/assets/**/*',
-}
+};
 
 // Paths to built files destinations
 const dest = {
@@ -39,7 +44,7 @@ const dest = {
 	scripts: 'build',
 	styles: 'build',
 	assets: 'build/assets',
-}
+};
 
 gulp.task('default', ['build', 'server', 'watch']);
 
@@ -52,23 +57,23 @@ gulp.task('clean', () => del('build'));
 gulp.task('assets', () => {
 	return gulp.src(src.assets)
 		.pipe(gulp.dest(dest.assets));
-})
+});
 
 gulp.task('html', () => {
 	return gulp.src(src.htmlEntry)
 		.pipe(gulp.dest(dest.htmlEntry));
-})
+});
 
 gulp.task('styles', () => {
 	return gulp.src(src.styles)
-		.pipe(sourcemaps.init())
+		.pipe(startSourcemapIfNotProduction())
 		.pipe(sass({outputStyle: 'compressed'})
 			.on('error', sass.logError))
 		.pipe(autoprefixer({
 			browsers: ['last 4 versions'],
 			cascade: true,
 		}))
-		.pipe(sourcemaps.write())
+		.pipe(endSourcemapIfNotProduction())
 		.pipe(gulp.dest(dest.styles));
 });
 
@@ -81,16 +86,16 @@ gulp.task('scripts', () => {
 		.pipe(source('bundle.js'))
 		.pipe(gulp.dest(dest.scripts))
 		.pipe(buffer())
-		.pipe(sourcemaps.init({loadMaps: true}))
+		.pipe(startSourcemapIfNotProduction())
 			.on('error', handleError)
 			.pipe(uglify())
 			.on('error', handleError)
-		.pipe(sourcemaps.write())
+		.pipe(endSourcemapIfNotProduction())
 		.pipe(gulp.dest(dest.scripts))
 });
 
 gulp.task('server', () => {
-	exec(`node ${src.serverEntry}`, (err, stdout, stderr) => {
+	exec(`node ${config.server}`, (err, stdout, stderr) => {
 		console.log(stdout);
 		console.log(stderr);
 	});
@@ -102,6 +107,14 @@ gulp.task('watch', () => {
 	gulp.watch(src.assets, ['assets']);
 	gulp.watch(src.htmlEntry, ['html']);
 });
+
+const startSourcemapIfNotProduction = () => {
+	return config.production ? gutil.noop() : sourcemaps.init({loadMaps: true});
+}
+
+const endSourcemapIfNotProduction = () => {
+	return config.production ? gutil.noop() : sourcemaps.write();
+}
 
 const handleError = function (...args) {
   notify.onError({
