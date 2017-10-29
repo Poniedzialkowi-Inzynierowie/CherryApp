cf6a04babf560618e9dd47036f726ad1031b908b
Jan <j.szachno@gmail.com>
2017-10-29 20:16:35 +0100

    added helmet security middleware to server file

--- server/main.js
+++ server/main.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const helmet = require('helmet');
 const compression = require('compression');
 
 const port = process.env.PORT || 8080;
 
 const app = express();
 
+app.use(helmet());
 app.use(express.static('build'));
 // gzip returned files for better speed
 app.use(compression());
