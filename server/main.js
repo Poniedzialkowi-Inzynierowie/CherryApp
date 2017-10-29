4f3daaec0e946bdef88c0cff54c5a427aa920f04
Jan <j.szachno@gmail.com>
2017-10-29 05:42:48 +0100

    making main server file super simple

--- server/main.js
+++ server/main.js
@@ -4,41 +4,6 @@ const port = process.env.PORT || 8080;
 
 const app = express();
 
-app.use(cookieParser());
-app.use(bodyParser.json()); // support json encoded bodies
-app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+app.use(express.static('build'))
 
-function getFile(path) {
-	fs.readFile(path, 'utf8', (err, data) => {
-		if (err) { console.log("Can't read file due to: "+ err) }
-		else{ return data }
-	});
-}
-
-
-// ----------------------------------------------------------------------------
-// As project is using REST api philosophy, we do not need
-// any dynamic access to static files and so we provide statically
-// typed routes to main.min.css and app.min.js
-
-app.route('/static')
-.get((req, res) => {
-	switch(req.param("file")){
-		case 'main.min.css':
-			res.send( getFile('static/main.min.css') );
-			break;
-		case 'app.min.js':
-			res.send( getFile('static/main.min.css') );
-			break;
-		default:
-			console.log(`Warning: Requested static file "${req.param('file')}" which doesn't exist, or is not served by the server`);
-	}
-})
-
-// ----------------------------------------------------------------------------
-http.createServer(app).listen(80);
-
-app.listen = function() {
-  var server = http.createServer(this);
-  return server.listen.apply(server, arguments);
-};
+app.listen(port);
