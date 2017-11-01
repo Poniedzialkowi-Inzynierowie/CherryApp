911cb12483e47a4166399183fb17b729340cb3ee
Illicitly <selfish570@gmail.com>
2017-11-01 22:01:31 +0100

    Added registering to service worker

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const { h, render, Component } = require('preact');
 const Test = require('./components/Test');
-
+require('./static/reg.js')
 class App extends Component {
     render() {
         return <Test />;
