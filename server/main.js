8f6415d4f0d91d2e551fb8df2472ed27f08473d6
Jan <j.szachno@gmail.com>
2017-11-04 11:22:01 +0100

    adapted server to standards

--- server/main.js
+++ server/main.js
@@ -1,16 +1,16 @@
-const express = require('express');
-const helmet = require('helmet');
-const compression = require('compression');
+const express = require('express')
+const helmet = require('helmet')
+const compression = require('compression')
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8080
 
-const app = express();
+const app = express()
 
 // set http security headers
-app.use(helmet());
+app.use(helmet())
 // serve static files from build folder
-app.use(express.static('build'));
+app.use(express.static('build'))
 // gzip returned files for better speed
-app.use(compression());
+app.use(compression())
 
-app.listen(port);
+app.listen(port)
