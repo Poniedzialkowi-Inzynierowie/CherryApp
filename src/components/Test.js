21089589e430ccb27624bb9c38ce7e2aaa3780a6
Jan <j.szachno@gmail.com>
2017-11-04 14:34:06 +0100

    replaced test component with basic clock

--- src/components/Test.js
+++
@@ -1,9 +0,0 @@
-const { h, render, Component } = require('preact') // eslint-disable-line no-unused-vars
-
-class Test extends Component {
-  render () {
-    return <h1>Hello world!</h1>
-  }
-}
-
-module.exports = Test
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 const { h, render, Component } = require('preact')
-const Test = require('./components/Test')
+const Clock = require('./components/Clock')
 require('./static/reg.js')
 class App extends Component {
   render () {
-    return <Test />
+    return <Clock />
   }
 }
 
