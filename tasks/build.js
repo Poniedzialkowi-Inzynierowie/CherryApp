42f8c0629ded06f83d219511fe8db6deec9a238b
Kacper Skrzyński <adamn.zxc@gmail.com>
2017-11-16 23:07:05 +0100

    Resolved half of the problem. Added forgotten comment

--- tasks/build.js
+++ tasks/build.js
@@ -5,28 +5,27 @@ module.exports = (gulp, releaseStage) => {
 		_cacher: require('gulp-sw-cache'),
 		_watcher: require('gulp-watch'),
 
+		/* gulp-watch have some problem with running same task in the row and so
+		for now it's not switched on by default due to his blocking behavior */
 		_watch: (path) => releaseStage
 			? gulp.src(path)
 			: this._watcher(path),
 
 		// ------------------------------------------------------
 
-		clear: () =>
+		clear:
 			this._cleaner('./build'),
-		build: () => {
-			return this._watch('src/main.js')
+		build:
+			this._watch('src/main.js')
 				.pipe(this._builder())
-				.pipe(gulp.dest('./build'))
-		},
-		copy: () => {
-			return this._watch('src/main.js')
-				.pipe(gulp.dest('./build'))
-		},
-		cache: () => {
-			return this._watch('./build/*')
+				.pipe(gulp.dest('./build')),
+		copy:
+			this._watch('src/main.js')
+				.pipe(gulp.dest('./build')),
+		cache:
+			this._watch('./build/*')
 				.pipe(this._cacher({exclude: 'admin.html'}))
 				.pipe(gulp.dest('./build'))
-		}
 	}
 	return gulp.series(task.clear)
 }
