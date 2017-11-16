f56de209b57715728459568ed9828afae1e38879
Kacper Skrzyński <adamn.zxc@gmail.com>
2017-11-16 22:59:03 +0100

    deleted no longer important comment and added missing tasks description along with new and last task ('sslSupport')

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,29 @@
 const gulp = require('gulp')
 const releaseStage = process.env.NODE_ENV === 'production'
 
-/* gulp-watch have some problem with running same task in the row and so
-  for now it's not switched on by default due to his blocking behavior */
-
 function getTask (task) { return require('./tasks/' + task)(gulp, releaseStage) }
 
 // TODO: setup unit testing for server and components
 // TODO: add external config where development localhost port is set
 
 function build () {
-	this.displayName = ''
-	this.description = ''
+	this.displayName = 'Build'
+	this.description = 'Build all files from which frontend is made of'
 	return getTask('build')
 }
 function server () {
-	this.displayName = 'runServer'
-	this.description = 'Thankes to this function, depending on release stage, an adequate server is deployed'
+	this.displayName = 'sslSupport'
+	this.description = 'Generate keys, and certificates so server can support ssl'
+	return getTask('ssl')
+}
+function server () {
+	this.displayName = 'Server'
+	this.description = 'Thanks to this function, depending on release stage, an adequate server is deployed'
 	return getTask('server')
 }
+
 gulp.task(build)
+// gulp.task(ssl)
 gulp.task(server)
-gulp.task('default', gulp.series('build', 'server'))
+
+gulp.task('default', gulp.series('build','ssl', 'server'))
